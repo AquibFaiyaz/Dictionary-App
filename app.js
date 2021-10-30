b674252bda77f2893bed11619c1feaded1ff81d2
Aquib Faiyaz <faiyazaquib28@gmail.com>
2021-10-30 13:52:04 +0530

    make it better

--- app.js
+++ app.js
@@ -13,10 +13,11 @@ app.use(express.json());
 app.use("/api/v1/dictionary", words);
 
 const url = process.env.MONGO_URI;
+const port = process.env.port || 8000;
 const start = async () => {
   try {
     await connectDB(url);
-    app.listen(8000, () => {
+    app.listen(port, () => {
       console.log("server listening on port 8000...");
     });
   } catch (error) {
