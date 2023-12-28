e47d2977d2da89654885da3fd056e9153def3142
Aquib Faiyaz <faiyazaquib28@gmail.com>
2023-12-28 22:50:06 +0530

    added domain

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require("morgan");
 const connectDB = require("./db/connect");
 const cors = require("cors");
 
@@ -12,6 +13,8 @@ app.use(express.static("../client/dictionary/public"));
 app.use(cors());
 app.use(express.json());
 
+app.use(morgan("dev"));
+
 app.use("/api/v1/dictionary", words);
 
 const url = process.env.MONGO_URI;
@@ -19,7 +22,7 @@ const port = process.env.PORT || 8000;
 const start = async ({ dbConnectUrl, apiPort }) => {
   try {
     await connectDB(dbConnectUrl);
-    app.listen(apiPort, "0.0.0.0", () => {
+    app.listen(apiPort, "212.227.243.53", () => {
       console.log(`Server is listening on ${port}`);
     });
   } catch (error) {
